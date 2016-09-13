16e9f6f4d10c408a44c55e28cbca4620b2134b21
Jason Van Hil <jasonvanhil@gmail.com>
2016-09-13 17:49:17 +0100

    Update

--- vc/templates/ps_button.php
+++ vc/templates/ps_button.php
@@ -15,11 +15,30 @@ $inline_css = $wrapper_attributes = array();
 
 $atts = vc_map_get_attributes( $this->getShortcode(), $atts ); 
 
-
+$slide_title = array('Phasellus posuere mattis ligula id consectetur.','Phasellus posuere mattis ligula id consectetur','Nullam pellentesque blandit dolor','Lorem ipsum dolor sit amet, consectetur adipiscing elit');
+$button_text = array('watch the video','read more','learn more','click here');
 ?>
 
-	<a href="<?php echo esc_url($atts['url']); ?>" class="xxxxxxxxl <?php echo esc_attr($atts['type']); ?>">
-		<span><?php echo esc_attr($content); ?></span>
-	</a>
+<div class="paca-full-height">
+	<ul class="paca-slider papacig_slider" data-slick='{"infinite":true,"dots":true,"duration":6000,"autoplay":true,"autoplaySpeed":6000}' style="display:none;">
+		<?php $i=0; while($i <= 4){ ?>
+		<li>
+			<div class="wrap">
+				<div class="contents align-centre">
+					<h2><?php  echo $slide_title[array_rand($slide_title)]; ?></h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus posuere mattis ligula id consectetur. Nullam pellentesque blandit dolor, eleifend rhoncus magna rutrum… Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus posuere mattis ligula id consectetur. Nullam pellentesque blandit dolor, eleifend rhoncus magna rutrum… Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus posuere mattis ligula id consectetur. Nullam pellentesque blandit dolor, eleifend rhoncus magna rutrum… Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus posuere mattis ligula id consectetur. Nullam pellentesque blandit dolor, eleifend rhoncus magna rutrum…</p>
+					<br>
+					<a href="#" title="title" alt="alt here" class="paca-button paca-button-white rounded bordered hover-right large"><span class="text"><?php  echo $button_text[array_rand($button_text)]; ?></span><i class="paca-icon glyphicon glyphicon-chevron-right" aria-hidden="true"></i></a>
+				</div>
+			</div>
+			<div class="featured-image" style="background-image:url(<?php echo get_template_directory_uri() ?>/assets/img/xx-large-<?php echo rand(1,5) ?>.jpg)"><img src="<?php echo get_template_directory_uri() ?>/assets/img/xx-large-<?php echo rand(1,5) ?>.jpg" class="img-responsive"></div>
+		</li>
+		<?php $i++;} ?>
+
+	</ul>
+	<div class="paca-cursor-down">
+		<i class="fa fa-angle-down fa-2x" aria-hidden="true" data-speed="1000"></i>
+	</div>	
+</div>
 
 <?php
